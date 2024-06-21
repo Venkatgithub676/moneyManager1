3863cc766075e71fe663f4022c04d298772f2806
venkat <venkat>
2024-06-21 14:01:46 +0000

    Fourth Commit

--- src/components/MoneyManager/index.js
+++ src/components/MoneyManager/index.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
 import MoneyDetails from '../MoneyDetails'
+import TransactionItem from '../TransactionItem'
 import './index.css'
 
 const transactionTypeOptions = [
@@ -16,6 +18,98 @@ const transactionTypeOptions = [
 // Write your code here
 
 class MoneyMangager extends Component {
+  state = {
+    listItems: [],
+    title: '',
+    amt: '',
+    type: 'Income',
+  }
+
+  onChangeTitle = event => {
+    this.setState({title: event.target.value})
+  }
+
+  onChangeAmt = event => {
+    this.setState({amt: event.target.value})
+  }
+
+  onChangeSelect = event => {
+    this.setState({type: event.target.value})
+  }
+
+  submitForm = event => {
+    event.preventDefault()
+    const {title, amt, type} = this.state
+    const newObj = {
+      id: uuidv4(),
+      title,
+      amt,
+      type,
+      clickedDelBtn: false,
+    }
+    this.setState(prevState => ({listItems: [...prevState.listItems, newObj]}))
+  }
+
+  formContainer = () => (
+    <form className="add-transaction-con" onSubmit={this.submitForm}>
+      <h1 className="add-transaction-heading">Add Transaction</h1>
+      <label htmlFor="title" className="label">
+        Title
+      </label>
+      <br />
+      <input
+        type="text"
+        onChange={this.onChangeTitle}
+        id="title"
+        className="transaction-input"
+      />
+      <br />
+      <label className="label" htmlFor="amount">
+        Amount
+      </label>
+      <br />
+      <input
+        type="text"
+        id="amount"
+        onChange={this.onChangeAmt}
+        className="transaction-input"
+      />
+      <br />
+      <label className="label" htmlFor="select">
+        Type
+      </label>
+      <br />
+      <select id="select" className="select-el" onChange={this.onChangeSelect}>
+        {transactionTypeOptions.map(each => (
+          <option value={each.displayText}>{each.displayText}</option>
+        ))}
+      </select>
+      <br />
+      <button type="submit" className="add-btn">
+        Add
+      </button>
+    </form>
+  )
+
+  historyContainer = () => {
+    const {listItems} = this.state
+    return (
+      <div className="history-heading-con">
+        <h1 className="history-heading">History</h1>
+        <div className="history-con">
+          <h1 className="headings">Title</h1>
+          <h1 className="headings">Amount</h1>
+          <h1 className="headings">Type</h1>
+        </div>
+        <ul className="ul-con">
+          {listItems.map(each => (
+            <TransactionItem each={each} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className="bg-container">
@@ -23,38 +117,15 @@ class MoneyMangager extends Component {
           <div className="name-con">
             <h1 className="name">Hi, Richard</h1>
             <p className="welcome">
-              Welcome back to your{' '}
+              Welcome back to your
               <span className="money-manager">Money Manager</span>
             </p>
           </div>
           <MoneyDetails />
-          <form onSubmit="" className="add-transaction-con">
-            <h1 className="add-transaction-heading">Add Transaction</h1>
-            <label htmlFor="title" className="label">
-              Title
-            </label>
-            <br />
-            <input type="text" id="title" className="transaction-input" />
-            <br />
-            <label className="label" htmlFor="amount">
-              Amount
-            </label>
-            <br />
-            <input type="text" id="amount" className="transaction-input" />
-            <br />
-            <label className="label" htmlFor="select">
-              Type
-            </label>
-            <br />
-            <select id="select" className="select-el">
-              <option value="INCOME">Income</option>
-              <option value="Expenses">Expenses</option>
-            </select>
-            <br />
-            <button type="submit" className="add-btn">
-              Add
-            </button>
-          </form>
+          <div className="form-history-con">
+            {this.formContainer()}
+            {this.historyContainer()}
+          </div>
         </div>
       </div>
     )
--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,1 +1,29 @@
 // Write your code here
+
+import './index.css'
+
+const TransactionItem = props => {
+  const {each, delBtn} = props
+  const {id, title, amt, type} = each
+
+  const onClickDelBtn = () => {
+    delBtn(id)
+  }
+
+  return (
+    <li className="li-con">
+      <p className="li-para">{title}</p>
+      <p className="li-para">{amt}</p>
+      <p className="li-para">{type}</p>
+      <button className="del-btn" type="button" onClick={onClickDelBtn}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png"
+          alt="delete"
+          className="del-img"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default TransactionItem
